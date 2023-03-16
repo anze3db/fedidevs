7e28e88042c840e3099d879fbd497efb301c31e3
Anže Pečar <anze@pecar.me>
2023-03-16 21:52:22 +0000

    Customize the admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 
 from accounts.models import Account
 
 
 @admin.register(Account)
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "username",
+        "display_name",
+        "followers_count",
+        "following_count",
+        "statuses_count",
+        "bot",
+        "locked",
+        "discoverable",
+        "last_sync_at",
+        "html_note",
+    )
+    search_fields = (
+        "username",
+        "note",
+    )
+
+    def html_note(self, obj):
+        return mark_safe(obj.note)
