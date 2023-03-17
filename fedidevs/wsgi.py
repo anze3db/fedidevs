398654e0fd8d04b44d40451fb9b8b89b9ae30e4a
Anže Pečar <anze@pecar.me>
2023-03-17 17:02:00 +0000

    Rename devtribe -> fedidevs

--- fedidevs/wsgi.py
+++ fedidevs/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for devtribe project.
+WSGI config for fedidevs project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devtribe.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fedidevs.settings")
 
 application = get_wsgi_application()
