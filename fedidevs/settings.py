3f95861fceb9da9c9d9ee1351050de4807213868
Anže Pečar <anze@pecar.me>
2023-04-15 05:52:13 +0100

    Add CSRF_TRUSTED_ORIGINS

--- fedidevs/settings.py
+++ fedidevs/settings.py
@@ -41,6 +41,10 @@ DEBUG = env.bool("DEBUG", default=True)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 
+CSRF_TRUSTED_ORIGINS = env.list(
+    "CSRF_TRUSTED_ORIGINS", default=["http://127.0.0.1", "http://localhost"]
+)
+
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
