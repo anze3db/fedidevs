3f2afbca8243c2c54b24446383fc83b8eb1284cf
Anže Pečar <anze@pecar.me>
2023-05-19 16:46:55 +0100

    Remove exception

--- accounts/management/commands/optimizer.py
+++ accounts/management/commands/optimizer.py
@@ -7,14 +7,7 @@ class Command(RichCommand):
     help = "Crawles the fosstodon.org API and saves all accounts to the database"
 
     def handle(self, *args, **options):
-        ids = list(
-            AccountLookup.objects.values_list("account_id", flat=True).distinct()
-        )
-        ids += list(
-            Account.objects.filter(instance="oldbytes.space").values_list(
-                "account_id", flat=True  # Don't delete oldbytes.space accounts
-            )
-        )
+        ids = AccountLookup.objects.values_list("account_id", flat=True).distinct()
         total = Account.objects.exclude(id__in=ids).count()
         self.console.print(f"Deleting {total}")
         Account.objects.exclude(id__in=ids).delete()
