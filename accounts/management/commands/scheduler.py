5fa4c12c7724925c3d354713fd814c8b4a640230
Anže Pečar <anze@pecar.me>
2023-09-21 07:37:48 +0100

    Fix for monitor failuer

--- accounts/management/commands/scheduler.py
+++ accounts/management/commands/scheduler.py
@@ -9,20 +9,20 @@ from sentry_sdk.crons import monitor
 class Command(RichCommand):
     help = "Starts the scheduler"
 
-    @monitor(monitor_slug="daily-sync")
     def job(self):
-        self.console.print("Starting daily job")
-        self.console.print("Running crawler")
-        management.call_command("crawler", skip_inactive_for=3, pre_filter=True)
-        self.console.print("Running indexer")
-        management.call_command("indexer")
-        self.console.print("Running optimizer")
-        management.call_command("optimizer")
-        self.console.print("Running statuser")
-        management.call_command("statuser")
-        self.console.print("Running dailystats")
-        management.call_command("dailystats")
-        self.console.print("All done! 🎉")
+        with monitor(monitor_slug="daily-sync"):
+            self.console.print("Starting daily job")
+            self.console.print("Running crawler")
+            management.call_command("crawler", skip_inactive_for=3, pre_filter=True)
+            self.console.print("Running indexer")
+            management.call_command("indexer")
+            self.console.print("Running optimizer")
+            management.call_command("optimizer")
+            self.console.print("Running statuser")
+            management.call_command("statuser")
+            self.console.print("Running dailystats")
+            management.call_command("dailystats")
+            self.console.print("All done! 🎉")
 
     def handle(self, *args, **options):
         self.console.print("Starting scheduler 🕐")
