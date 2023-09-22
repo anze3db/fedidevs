da8a83a2e6a2e94980b8989fa6a7d5920dfbf7c3
Anže Pečar <anze@pecar.me>
2023-09-22 11:04:32 +0100

    Add more monitors because having 1 doesn't work

--- accounts/management/commands/scheduler.py
+++ accounts/management/commands/scheduler.py
@@ -9,20 +9,25 @@ from sentry_sdk.crons import monitor
 class Command(RichCommand):
     help = "Starts the scheduler"
 
+    @monitor(monitor_slug="daily-sync")
     def job(self):
-        with monitor(monitor_slug="daily-sync"):
-            self.console.print("Starting daily job")
-            self.console.print("Running crawler")
+        self.console.print("Starting daily job")
+        self.console.print("Running crawler")
+        with monitor(monitor_slug="daily-sync-crawler"):
             management.call_command("crawler", skip_inactive_for=3, pre_filter=True)
-            self.console.print("Running indexer")
+        self.console.print("Running indexer")
+        with monitor(monitor_slug="daily-sync-indexer"):
             management.call_command("indexer")
-            self.console.print("Running optimizer")
+        self.console.print("Running optimizer")
+        with monitor(monitor_slug="daily-sync-optimizer"):
             management.call_command("optimizer")
-            self.console.print("Running statuser")
+        self.console.print("Running statuser")
+        with monitor(monitor_slug="daily-sync-statuser"):
             management.call_command("statuser")
-            self.console.print("Running dailystats")
+        self.console.print("Running dailystats")
+        with monitor(monitor_slug="daily-sync-stats"):
             management.call_command("dailystats")
-            self.console.print("All done! 🎉")
+        self.console.print("All done! 🎉")
 
     def handle(self, *args, **options):
         self.console.print("Starting scheduler 🕐")
