0d217fcfba0d6f2c5ad5992f03d237a491749202
Anže Pečar <anze@pecar.me>
2023-10-18 13:25:43 +0100

    Add instance information to account

--- accounts/templatetags/account_extras.py
+++ accounts/templatetags/account_extras.py
@@ -12,3 +12,11 @@ def render_emojis(msg: str, emoji_list: list[str]):
             f"<img src='{emoji['url']}' class='emojione' alt='{emoji['shortcode']}' title='{emoji['shortcode']}' />",
         )
     return mark_safe(msg)
+
+
+@register.simple_tag
+def render_username_at_instance(account_url: str):
+    # account_url: https://fosstodon.org/@djangocon
+    # return: @djangocon@fosstodon.org
+    instance, username = account_url.replace("https://", "").split("/")
+    return f"{username}@{instance}"
