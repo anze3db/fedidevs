593b251693fc6199d5108b583b188f72159a5e52
Anže Pečar <anze@pecar.me>
2023-10-23 17:45:43 +0100

    Bulk update

--- accounts/management/commands/statuser.py
+++ accounts/management/commands/statuser.py
@@ -27,46 +27,9 @@ class Command(RichCommand):
         async with httpx.AsyncClient() as client:
             # batch by 100 requests
             for i in range(0, len(accounts), 100):
-                results = await asyncio.gather(
+                await asyncio.gather(
                     *[self.fetch(client, account) for account in accounts[i : i + 100]]
                 )
-                for account, posts in results:
-                    for result in posts:
-                        await Post.objects.aupdate_or_create(
-                            post_id=result["id"],
-                            account=account,
-                            defaults={
-                                "created_at": datetime.fromisoformat(
-                                    result["created_at"]
-                                ),
-                                "instance": account.instance,
-                                "in_reply_to_id": result["in_reply_to_id"],
-                                "in_reply_to_account_id": result[
-                                    "in_reply_to_account_id"
-                                ],
-                                "sensitive": result.get("sensitive", None),
-                                "spoiler_text": result["spoiler_text"],
-                                "visibility": result["visibility"],
-                                "language": result["language"],
-                                "uri": result["uri"],
-                                "url": result["url"],
-                                "replies_count": result["replies_count"],
-                                "reblogs_count": result["reblogs_count"],
-                                "favourites_count": result["favourites_count"],
-                                "edited_at": datetime.fromisoformat(result["edited_at"])
-                                if result["edited_at"]
-                                else None,
-                                "content": result["content"],
-                                "reblog": result["reblog"],
-                                "application": result.get("application", None),
-                                "media_attachments": result["media_attachments"],
-                                "mentions": result["mentions"],
-                                "tags": result["tags"],
-                                "emojis": result["emojis"],
-                                "card": result["card"],
-                                "poll": result["poll"],
-                            },
-                        )
                 self.console.print(f"Batch {i//100} done, sleeping for 90s")
                 await asyncio.sleep(
                     90
@@ -74,27 +37,84 @@ class Command(RichCommand):
 
     async def fetch(self, client, account: Account):
         try:
-            response = (
-                await client.get(
-                    f"https://{account.instance}/api/v1/accounts/{account.account_id}/statuses",
-                    params={
-                        "q": "python",
-                        "type": "statuses",
-                        "account_id": account.account_id,
-                    },
-                    timeout=30,
-                )
+            response = await client.get(
+                f"https://{account.instance}/api/v1/accounts/{account.account_id}/statuses",
+                params={
+                    "q": "python",
+                    "type": "statuses",
+                    "account_id": account.account_id,
+                },
+                timeout=30,
             )
             if response.status_code == 429:
                 self.console.print("Rate limited, sleeping for 5 minutes")
                 await asyncio.sleep(60 * 5)
-                return await self.fetch(client, account)
+                await self.fetch(client, account)
+                return
             if response.status_code != 200:
                 self.console.print(
                     f"[bold red]Error status code[/bold red] for {account}. {response.status_code}"
                 )
-                return account, []
-            return account, response.json()
+                return
+            posts = (
+                Post(
+                    post_id=result["id"],
+                    account=account,
+                    created_at=datetime.fromisoformat(result["created_at"]),
+                    instance=account.instance,
+                    in_reply_to_id=result["in_reply_to_id"],
+                    in_reply_to_account_id=result["in_reply_to_account_id"],
+                    sensitive=result.get("sensitive", None),
+                    spoiler_text=result["spoiler_text"],
+                    visibility=result["visibility"],
+                    language=result["language"],
+                    uri=result["uri"],
+                    url=result["url"],
+                    replies_count=result["replies_count"],
+                    reblogs_count=result["reblogs_count"],
+                    favourites_count=result["favourites_count"],
+                    edited_at=datetime.fromisoformat(result["edited_at"])
+                    if result["edited_at"]
+                    else None,
+                    content=result["content"],
+                    reblog=result["reblog"],
+                    application=result.get("application", None),
+                    media_attachments=result["media_attachments"],
+                    mentions=result["mentions"],
+                    tags=result["tags"],
+                    emojis=result["emojis"],
+                    card=result["card"],
+                    poll=result["poll"],
+                )
+                for result in response.json()
+            )
+
+            await Post.objects.abulk_create(
+                posts,
+                update_conflicts=True,
+                unique_fields=["post_id", "account"],
+                update_fields=[
+                    "account",
+                    "sensitive",
+                    "spoiler_text",
+                    "visibility",
+                    "language",
+                    "uri",
+                    "url",
+                    "replies_count",
+                    "reblogs_count",
+                    "favourites_count",
+                    "edited_at",
+                    "content",
+                    "reblog",
+                    "application",
+                    "media_attachments",
+                    "mentions",
+                    "tags",
+                    "emojis",
+                    "card",
+                    "poll",
+                ],
+            )
         except httpx.HTTPError:
             self.console.print(f"[bold red]Error timeout[/bold red] for {account}")
-            return account, []
