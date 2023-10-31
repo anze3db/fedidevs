795420486377cb8f99ad1b6fa1322701a51e9761
Anže Pečar <anze@pecar.me>
2023-10-31 18:35:24 +0000

    Remove unused import

--- fedidevs/urls.py
+++ fedidevs/urls.py
@@ -13,8 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import datetime as dt
-
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import include, path, register_converter
