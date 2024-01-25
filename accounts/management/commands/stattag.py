3750924168f15a6cb6a1351a69d0f2a27d8acf25
Anže Pečar <anze@pecar.me>
2024-01-25 11:12:55 +0000

    Fix typo

--- accounts/management/commands/stattag.py
+++ accounts/management/commands/stattag.py
@@ -195,7 +195,7 @@ class Command(RichCommand):
             conf_tags = [c.strip().replace("#", "").lower() for c in conf.tags.split(",")]
             posts = []
             for post in post_objs:
-                if conf.post_start_date and post.created_at.date() < conf.post_start_date:
+                if conf.posts_after and post.created_at.date() < conf.posts_after:
                     continue
                 for tag in post.tags:
                     if tag["name"].lower() in conf_tags:
