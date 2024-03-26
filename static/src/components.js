11f644d349052ac3d59704501eecd46916913237
Ivan Križnar <ivan@simplified.me>
2024-03-26 23:58:09 +0100

    WIP

---
+++ static/src/components.js
@@ -0,0 +1,40 @@
+// if (
+// 	localStorage.theme === "dark" ||
+// 	(!("theme" in localStorage) &&
+// 		window.matchMedia("(prefers-color-scheme: dark)").matches)
+// ) {
+// 	document.documentElement.classList.add("dark");
+// } else {
+// 	document.documentElement.classList.remove("dark");
+// }
+
+// localStorage.theme = "light";
+// localStorage.theme = "dark";
+
+// localStorage.removeItem("theme");
+
+class ThemeToggle extends HTMLElement {
+	constructor() {
+		super();
+		this.button = this.querySelector("button");
+		this.button.addEventListener("click", this.toggleTheme.bind(this));
+		if (
+			localStorage.theme === "dark" ||
+			(!("theme" in localStorage) &&
+				window.matchMedia("(prefers-color-scheme: dark)").matches)
+		) {
+			document.documentElement.classList.add("dark");
+		} else {
+			document.documentElement.classList.remove("dark");
+		}
+	}
+	toggleTheme() {
+		document.documentElement.classList.toggle("dark");
+		localStorage.setItem(
+			"theme",
+			document.documentElement.classList.contains("dark") ? "dark" : "light",
+		);
+	}
+}
+
+customElements.define("theme-toggle", ThemeToggle);
