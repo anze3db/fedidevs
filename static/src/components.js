2a9fd8959e7cb93b434ff69799ffbeda1111989e
Ivan Križnar <ivan@simplified.me>
2024-03-27 16:45:14 +0100

    Set appropriate dark/light mode instantly

--- static/src/components.js
+++ static/src/components.js
@@ -1,3 +1,12 @@
+if (
+	localStorage.getItem("theme") === "dark" ||
+	(!("theme" in localStorage) &&
+		window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+	document.documentElement.classList.add("dark");
+} else {
+	document.documentElement.classList.remove("dark");
+}
 
 class ThemeToggle extends HTMLElement {
 	constructor() {
