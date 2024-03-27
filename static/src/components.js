c29320def9dcd6e52f72dc65896a7d2e78c68383
Ivan Križnar <ivan@simplified.me>
2024-03-27 16:44:41 +0100

    Remove unused code

--- static/src/components.js
+++ static/src/components.js
@@ -1,17 +1,3 @@
-// if (
-// 	localStorage.theme === "dark" ||
-// 	(!("theme" in localStorage) &&
-// 		window.matchMedia("(prefers-color-scheme: dark)").matches)
-// ) {
-// 	document.documentElement.classList.add("dark");
-// } else {
-// 	document.documentElement.classList.remove("dark");
-// }
-
-// localStorage.theme = "light";
-// localStorage.theme = "dark";
-
-// localStorage.removeItem("theme");
 
 class ThemeToggle extends HTMLElement {
 	constructor() {
