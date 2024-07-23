f4e37fc6059e7c9302ba0237299b639e4a7ebc16
Gasper Martic <martic.gasper@gmail.com>
2024-07-23 12:46:46 +0200

    Reduce verbosity of console logs
    Console logs were very spammy when running certain commands.
    
    Signed-off-by: Gasper Martic <martic.gasper@gmail.com>

--- fedidevs/settings.py
+++ fedidevs/settings.py
@@ -252,7 +252,7 @@ LOGGING = {
     "disable_existing_loggers": False,
     "handlers": {
         "console": {
-            "level": "DEBUG",
+            "level": "INFO",
             "class": "logging.StreamHandler",
         },
         "file": {
