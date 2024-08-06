ff7d9493a786be0ba67b1dad4ba039bc173236f7
Anže Pečar <anze@pecar.me>
2024-08-06 14:11:43 +0100

    Improve the ai powered filters

--- accounts/management/commands/classify.py
+++ accounts/management/commands/classify.py
@@ -1,5 +1,4 @@
 import logging
-from collections import Counter
 
 from django.conf import settings
 from django_rich.management import RichCommand
@@ -11,11 +10,7 @@ logger = logging.getLogger(__name__)
 
 
 def current_cost(prompt_tokens, completion_tokens, prompt_token_price, completion_token_price):
-    logger.info("Prompt cost: %s", round(prompt_tokens * prompt_token_price, 6))
-    logger.info("Completion cost: %s", round(completion_tokens * completion_token_price, 6))
-    logger.info(
-        "Total cost: %s", round(prompt_tokens * prompt_token_price + completion_tokens * completion_token_price, 6)
-    )
+    return round(prompt_tokens * prompt_token_price + completion_tokens * completion_token_price, 6)
 
 
 class Command(RichCommand):
@@ -33,44 +28,45 @@ class Command(RichCommand):
         completion_token_price = 0.600 / 1_000_000
         prompt_token_price = 0.150 / 1_000_000
         processed = 0
+        total_accounts = AccountLookup.objects.count()
         while True:
-            accounts_lookup = AccountLookup.objects.filter(account_type="!")[:20]
+            accounts_lookup = AccountLookup.objects.filter(account_type="!")[:5]
             if not accounts_lookup:
                 logger.info("All done!")
-                current_cost(prompt_tokens, completion_tokens, prompt_token_price, completion_token_price)
                 break
-            pk_to_al = {al.pk: al for al in accounts_lookup}
-            lines = ["id,text"]
-            for account in accounts_lookup:
-                prompt = f"{account.pk},{account.text.replace(',', ' ').replace('\n', ' ')}"
-                lines.append(prompt)
-            completion = client.chat.completions.create(
-                messages=[
-                    {
-                        "role": "system",
-                        "content": "You are a CSV classifier. The input is a CSV file with two columns, id and text representing Mastodon accounts that are either humans or projects/companies/bots. For each account, you have to classify it as a human (H) if it has a name, surname, hobbies, refers to themselves in singular, uses I or has pronounes set. Classify as project (P) if the account is a project/company/bot and it refers to itself in plural (we) and doesn't have a name/surname. Output only the account id and the classification letter seperated by a comma.",
-                    },
-                    {
-                        "role": "user",
-                        "content": "\n".join(lines),
-                    },
-                ],
-                model="gpt-4o-mini",
-            )
-            res = completion.choices[0].message.content
-            completion_tokens += completion.usage.completion_tokens
-            prompt_tokens += completion.usage.prompt_tokens
             to_update = set()
-            cnt = Counter()
-            for out in res.splitlines():
-                if "," not in out:
+            for account in accounts_lookup:
+                prompt = f"{account.text.replace(',', ' ').replace('\n', ' ')}"
+                completion = client.chat.completions.create(
+                    messages=[
+                        {
+                            "role": "system",
+                            "content": "You are a CSV classifier. The input is Mastodon account text data including a bio, name, and link. You have to figure out if the account is a human or project/company/bot. Classify it as a human (H) if it has a name, surname, hobbies, refers to themselves in singular, uses I or has pronounes set. Classify as project (P) if the account is a project/company/bot and it refers to itself in plural (we) and doesn't have a name/surname. Output only the classification letter nothing else.",
+                        },
+                        {
+                            "role": "user",
+                            "content": prompt,
+                        },
+                    ],
+                    model="gpt-4o-mini",
+                )
+                res = completion.choices[0].message.content.strip()
+                if res not in ["H", "P"]:
+                    logger.error("Invalid classification %s", res)
                     continue
-                account_id, account_type = out.split(",")
-                cnt.update([account_type.strip()])
-                pk_to_al[int(account_id)].account_type = account_type.strip()
-                to_update.add(pk_to_al[int(account_id)])
-            AccountLookup.objects.bulk_update(accounts_lookup, ["account_type"])
+
+                completion_tokens += completion.usage.completion_tokens
+                prompt_tokens += completion.usage.prompt_tokens
+                account.account_type = res
+                to_update.add(account)
+            AccountLookup.objects.bulk_update(to_update, ["account_type"])
             processed += len(to_update)
-            logger.info(cnt)
-            current_cost(prompt_tokens, completion_tokens, prompt_token_price, completion_token_price)
-            logger.info("Processed %s accounts", processed)
+            cur_cst = current_cost(prompt_tokens, completion_tokens, prompt_token_price, completion_token_price)
+
+            logger.info(
+                "Processed %s accounts\ntotal_cost: %.6f\ncost per account: %.6f\nExpected total cost: %.6f",
+                processed,
+                cur_cst,
+                cur_cst / processed,
+                (cur_cst / processed) * total_accounts,
+            )
