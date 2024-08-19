0992b7219c67809734fed33746697eb2a535637e
Anže Pečar <anze@pecar.me>
2024-08-19 16:21:14 +0100

    Add text and og-image

--- stats/views.py
+++ stats/views.py
@@ -10,12 +10,16 @@ def stats(request):
     period = request.GET.get("p") or "weekly"
     if period == "weekly":
         graph_days_len = 7
+        period_name = "seven"
     elif period == "biweekly":
         graph_days_len = 14
+        period_name = "fourteen"
     elif period == "monthly":
         graph_days_len = 30
+        period_name = "thirty"
     else:
         graph_days_len = 7
+        period_name = "seven"
 
     cards = []
     values = [f"{lang.code}_accounts" for lang in (LANGUAGES + FRAMEWORKS)] + ["date"]
@@ -55,10 +59,11 @@ def stats(request):
         {
             "page_title": "Statistics | Fediverse Developers",
             "page": "stats",
-            "page_header": "Statistics",
-            "page_subheader": "",
-            "page_description": "",
-            "page_image": "og.png",
+            "page_header": "Stats",
+            "page_subheader": "See how many developers on Mastodon are using your favorite languages and frameworks.",
+            "page_description": "Statistics on the number of developers on Mastodon using various programming languages and frameworks.",
+            "page_image": "og-stats.png",
+            "period_name": period_name,
             "cards": cards,  # Needed for template rendering
             "cards_json": json.dumps(cards),  # Needed for JavaScript parsing
             "period_display_name": f"Last {graph_days_len} days",
