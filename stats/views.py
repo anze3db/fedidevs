43e2ca7d8256fbb0bc9c204b6e18ed8dc362b791
Gasper Martic <martic.gasper@gmail.com>
2024-08-19 09:56:41 +0200

    Add dropdown for selecting stats time period
    Signed-off-by: Gasper Martic <martic.gasper@gmail.com>

--- stats/views.py
+++ stats/views.py
@@ -5,12 +5,21 @@ from django.shortcuts import render
 from accounts.models import FRAMEWORKS, LANGUAGES
 from stats.models import Daily
 
-GRAPH_DAYS_LEN = 30
-
 
 def stats(request):
+    period = request.GET.get("p") or "monthly"
+    if period == "weekly":
+        graph_days_len = 7
+        period_display_name = "Last 7 days"
+    elif period == "monthly":
+        graph_days_len = 30
+        period_display_name = "Last 30 days"
+    elif period == "trimonthly":
+        graph_days_len = 90
+        period_display_name = "Last 90 days"
+
     cards = []
-    daily_objects = Daily.objects.order_by("-date")[:GRAPH_DAYS_LEN]
+    daily_objects = Daily.objects.order_by("-date")[:graph_days_len]
     for lang in LANGUAGES + FRAMEWORKS:
         card = {
             "code": lang.code,
@@ -29,9 +38,11 @@ def stats(request):
 
         start_count = getattr(daily_objects[len(daily_objects) - 1], f"{lang.code}_accounts")
         end_count = getattr(daily_objects[0], f"{lang.code}_accounts")
-        if start_count < end_count:
+        if start_count == 0:
+            card["percent_change"] = "N/A"
+        elif start_count < end_count:
             card["percent_change"] = round((end_count - start_count) / start_count * 100, 1)
-        if start_count > end_count:
+        elif start_count > end_count:
             card["percent_change"] = round(-((start_count - end_count) / start_count * 100), 2)
 
         card["total_accounts"] = end_count
@@ -50,5 +61,6 @@ def stats(request):
             "page_image": "og.png",
             "cards": cards,  # Needed for template rendering
             "cards_json": json.dumps(cards),  # Needed for JavaScript parsing
+            "period_display_name": period_display_name,
         },
     )
