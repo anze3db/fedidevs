db40c06ba4a0af7260ae336eca8044e89823fda8
Anže Pečar <anze@pecar.me>
2024-08-29 14:36:54 +0100

    Remove template profiler

--- fedidevs/settings.py
+++ fedidevs/settings.py
@@ -73,7 +73,6 @@ INSTALLED_APPS = [
     "heroicons",
     "django_browser_reload",
     "debug_toolbar",
-    "template_profiler_panel",
     "django_dramatiq",
     "django_tui",
     "template_partials.apps.SimpleAppConfig",
@@ -135,24 +134,6 @@ TEMPLATES = [
             ],
         },
     },
-]
-
-DEBUG_TOOLBAR_PANELS = [
-    "debug_toolbar.panels.history.HistoryPanel",
-    "debug_toolbar.panels.versions.VersionsPanel",
-    "debug_toolbar.panels.timer.TimerPanel",
-    "debug_toolbar.panels.settings.SettingsPanel",
-    "debug_toolbar.panels.headers.HeadersPanel",
-    "debug_toolbar.panels.request.RequestPanel",
-    "debug_toolbar.panels.sql.SQLPanel",
-    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
-    "debug_toolbar.panels.templates.TemplatesPanel",
-    "debug_toolbar.panels.alerts.AlertsPanel",
-    "debug_toolbar.panels.cache.CachePanel",
-    "debug_toolbar.panels.signals.SignalsPanel",
-    "debug_toolbar.panels.redirects.RedirectsPanel",
-    "debug_toolbar.panels.profiling.ProfilingPanel",
-    "template_profiler_panel.panels.template.TemplateProfilerPanel",
 ]
 
 STATICFILES_DIRS = [
