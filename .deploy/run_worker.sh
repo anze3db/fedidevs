3c950bc4911deda251e0e8c33ce043d78fc529d4
Anže Pečar <anze@pecar.me>
2024-10-11 20:44:43 +0100

    Remove frozen from one other place

--- .deploy/run_worker.sh
+++ .deploy/run_worker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 pushd "$(dirname "$0")/.."
-uv run --frozen python manage.py rundramatiq --processes 1 --threads 2
+uv run --locked python manage.py rundramatiq --processes 1 --threads 2
 popd
