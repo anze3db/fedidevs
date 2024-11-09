c90f29f81a952784b09bc7f1978790bceb893aa1
Anže Pečar <anze@pecar.me>
2024-11-09 00:40:49 +0000

    Stop using pid files

--- .deploy/update.sh
+++ .deploy/update.sh
@@ -5,7 +5,7 @@ git pull
 uv sync --locked
 uv run python manage.py collectstatic --noinput
 uv run python manage.py migrate
-sudo kill -hup `cat /var/run/gunicorn-fedidevs.pid`
+ps axf | grep 'gunicorn: master \[fedidevs\]' | awk '{print "sudo kill -hup " $1}' | sh
 sudo systemctl restart fedidevs-worker
 sudo systemctl restart fedidevs-scheduler
 echo `date "+%Y-%m-%d %H:%M:%S.%3N"` ' Updated' >> update.log
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,15 +1,4 @@
-# import multiprocessing
-
-pidfile = "/var/run/gunicorn-fedidevs.pid"
 proc_name = "fedidevs"
 bind = "unix:fedidevs.sock"
-workers = 4  # multiprocessing.cpu_count() + 1
-threads = 4  # multiprocessing.cpu_count() * 2
-# Access log - records incoming HTTP requests
-# accesslog = "/var/log/gunicorn-fedidevs.access.log"
-# Error log - records Gunicorn server goings-on
-# errorlog = "/var/log/gunicorn-fedidevs.error.log"
-# Whether to send Django output to the error log
-# capture_output = True
-# How verbose the Gunicorn error logs should be
-# loglevel = "info"
+workers = 4
+threads = 4
