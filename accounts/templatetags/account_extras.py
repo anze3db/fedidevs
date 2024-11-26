7d10ec998d7bba69cf97452faf63e39ae0604f7f
Anže Pečar <anze@pecar.me>
2024-11-26 15:17:01 +0000

    Support emojis

--- accounts/templatetags/account_extras.py
+++ accounts/templatetags/account_extras.py
@@ -1,7 +1,9 @@
 from django import template
+from django.templatetags.static import static
+from django.utils.html import escape
 from django.utils.safestring import mark_safe
 
-from accounts.models import Instance
+from accounts.models import FRAMEWORKS, LANGUAGES, Instance
 
 register = template.Library()
 
@@ -12,6 +14,17 @@ def render_emojis(msg: str, emoji_list: list[str]):
         msg = msg.replace(
             f":{emoji['shortcode']}:",
             f"<img src='{emoji['url']}' class='emojione' alt='{emoji['shortcode']}' title='{emoji['shortcode']}' />",
+        )
+    return mark_safe(msg)  # noqa: S308
+
+
+@register.simple_tag
+def render_language_emojis(msg: str):
+    msg = escape(msg)
+    for lang in LANGUAGES + FRAMEWORKS:
+        msg = msg.replace(
+            f":{lang.code}:",
+            f"<img src='{static(lang.image)}' class='emojione' alt=':{lang.code}:' title=':{lang.code}:' />",
         )
     return mark_safe(msg)  # noqa: S308
 
