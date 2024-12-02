616eb2e5043513f26830658cb3f48a11edd80c00
Anže Pečar <anze@pecar.me>
2024-12-02 01:22:04 +0000

    Reenable custom user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 from accounts.models import Account
+
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class MyUserAdmin(UserAdmin):
+    list_display = ("username", "last_login", "date_joined", "is_staff", "is_superuser")
 
 
 @admin.register(Account)
