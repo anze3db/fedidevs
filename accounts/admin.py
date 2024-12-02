00fd4c2ec287c524081a310ebfe574b9a724c037
Anže Pečar <anze@pecar.me>
2024-12-02 01:13:53 +0000

    Remove custom user admin override

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
 from accounts.models import Account
 
 admin.site.unregister(User)
-
-
-@admin.register(User)
-class MyUserAdmin(UserAdmin):
-    list_display = ("username", "last_login", "date_joined", "is_staff", "is_superuser")
 
 
 @admin.register(Account)
