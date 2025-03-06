6c1a2c9db7afd18dcea9ac062ce9bd5f8f474944
Anže Pečar <anze@pecar.me>
2025-03-06 02:04:14 +0000

    Improve batching

--- accounts/management/commands/crawler.py
+++ accounts/management/commands/crawler.py
@@ -57,107 +57,111 @@ class Command(RichCommand):
             start_time = timezone.now()
             instance_models = await Instance.objects.ain_bulk(field_name="instance")
             if instances:
-                to_index = instances.split(",")
+                all_to_index = instances.split(",")
             else:
-                to_index = [
+                all_to_index = [
                     i async for i in Instance.objects.filter(deleted_at__isnull=True).values_list("instance", flat=True)
                 ]
-            while to_index:
-                now = timezone.now()
-                results = await asyncio.gather(
-                    *[self.fetch(client, offset, instance, skip_inactive_for) for instance in to_index[:50]]
-                )
-                fetched_accounts = []
-                for instance, response in results:
-                    if not response:
-                        self.console.print(f"[green]Done with {instance}[/green]")
-                        to_index.remove(instance)
-                        continue
-                    inst = instance_models.get(instance)
-                    fetched_accounts += [
-                        Account(
-                            account_id=account["id"],
-                            instance=account["url"].split("/")[2],
-                            instance_model=inst,
-                            username=account["username"],
-                            username_at_instance=f"@{account['username'].lower()}@{inst.domain.lower()}",
-                            acct=account["acct"],
-                            display_name=account["display_name"],
-                            locked=account["locked"],
-                            bot=account["bot"],
-                            discoverable=account.get("discoverable", False),
-                            group=account.get("group", False),
-                            noindex=account.get("noindex", None),
-                            created_at=(dt.datetime.fromisoformat(account["created_at"])),
-                            last_status_at=convert_last_status_at(account.get("last_status_at")),
-                            last_sync_at=now,
-                            followers_count=account["followers_count"],
-                            following_count=account["following_count"],
-                            statuses_count=account["statuses_count"],
-                            note=account["note"],
-                            url=account["url"],
-                            avatar=account["avatar"],
-                            avatar_static=account["avatar_static"],
-                            header=account["header"],
-                            header_static=account["header_static"],
-                            emojis=account["emojis"],
-                            roles=account.get("roles", []),
-                            fields=account["fields"],
+            original_offset = offset
+            for batch in range(0, len(all_to_index), 50):
+                offset = original_offset
+                to_index = all_to_index[batch : batch + 50]
+                while to_index:
+                    now = timezone.now()
+                    results = await asyncio.gather(
+                        *[self.fetch(client, offset, instance, skip_inactive_for) for instance in to_index]
+                    )
+                    fetched_accounts = []
+                    for instance, response in results:
+                        if not response:
+                            self.console.print(f"[green]Done with {instance}[/green]")
+                            to_index.remove(instance)
+                            continue
+                        inst = instance_models.get(instance)
+                        fetched_accounts += [
+                            Account(
+                                account_id=account["id"],
+                                instance=account["url"].split("/")[2],
+                                instance_model=inst,
+                                username=account["username"],
+                                username_at_instance=f"@{account['username'].lower()}@{inst.domain.lower()}",
+                                acct=account["acct"],
+                                display_name=account["display_name"],
+                                locked=account["locked"],
+                                bot=account["bot"],
+                                discoverable=account.get("discoverable", False),
+                                group=account.get("group", False),
+                                noindex=account.get("noindex", None),
+                                created_at=(dt.datetime.fromisoformat(account["created_at"])),
+                                last_status_at=convert_last_status_at(account.get("last_status_at")),
+                                last_sync_at=now,
+                                followers_count=account["followers_count"],
+                                following_count=account["following_count"],
+                                statuses_count=account["statuses_count"],
+                                note=account["note"],
+                                url=account["url"],
+                                avatar=account["avatar"],
+                                avatar_static=account["avatar_static"],
+                                header=account["header"],
+                                header_static=account["header_static"],
+                                emojis=account["emojis"],
+                                roles=account.get("roles", []),
+                                fields=account["fields"],
+                            )
+                            for account in response
+                            if account.get("id")
+                        ]
+                    inserted_accounts = fetched_accounts
+                    try:
+                        await Account.objects.abulk_create(
+                            inserted_accounts,
+                            unique_fields=["account_id", "instance"],
+                            update_conflicts=["account_id", "instance"],
+                            update_fields=[
+                                "username",
+                                "username_at_instance",
+                                "acct",
+                                "display_name",
+                                "locked",
+                                "bot",
+                                "discoverable",
+                                "group",
+                                "noindex",
+                                "last_status_at",
+                                "last_sync_at",
+                                "followers_count",
+                                "following_count",
+                                "statuses_count",
+                                "note",
+                                "url",
+                                "avatar",
+                                "avatar_static",
+                                "header",
+                                "header_static",
+                                "emojis",
+                                "roles",
+                                "fields",
+                                "instance_model",
+                            ],
                         )
-                        for account in response
-                        if account.get("id")
-                    ]
-                inserted_accounts = fetched_accounts
-                try:
-                    await Account.objects.abulk_create(
-                        inserted_accounts,
-                        unique_fields=["account_id", "instance"],
-                        update_conflicts=["account_id", "instance"],
-                        update_fields=[
-                            "username",
-                            "username_at_instance",
-                            "acct",
-                            "display_name",
-                            "locked",
-                            "bot",
-                            "discoverable",
-                            "group",
-                            "noindex",
-                            "last_status_at",
-                            "last_sync_at",
-                            "followers_count",
-                            "following_count",
-                            "statuses_count",
-                            "note",
-                            "url",
-                            "avatar",
-                            "avatar_static",
-                            "header",
-                            "header_static",
-                            "emojis",
-                            "roles",
-                            "fields",
-                            "instance_model",
-                        ],
-                    )
-                except ProgrammingError:
-                    logger.warning(
-                        "Faled to insert",
-                        extra={
-                            "inserted_accounts": [
-                                f"{account.instance} {account.account_id}" for account in inserted_accounts
-                            ]
-                        },
-                    )
-                    inserted_accounts = []
-                if fetched_accounts:
-                    self.console.print(
-                        f"Fetched {len(fetched_accounts)}, inserted {len(inserted_accounts)}. Current offset {offset}. Max last_status_at {naturaltime(max(account.last_status_at for account in fetched_accounts if account.last_status_at))}"
-                    )
-                offset += 1
-            self.console.print(
-                f"Done. Started at {start_time}. Ended at {timezone.now()}, duration {timezone.now() - start_time}"
-            )
+                    except ProgrammingError:
+                        logger.warning(
+                            "Faled to insert",
+                            extra={
+                                "inserted_accounts": [
+                                    f"{account.instance} {account.account_id}" for account in inserted_accounts
+                                ]
+                            },
+                        )
+                        inserted_accounts = []
+                    if fetched_accounts:
+                        self.console.print(
+                            f"Fetched {len(fetched_accounts)}, inserted {len(inserted_accounts)}. Current offset {offset}. Max last_status_at {naturaltime(max(account.last_status_at for account in fetched_accounts if account.last_status_at))}"
+                        )
+                    offset += 1
+                self.console.print(
+                    f"Done. Started at {start_time}. Ended at {timezone.now()}, duration {timezone.now() - start_time}"
+                )
 
     async def fetch(self, client, offset, instance, skip_inactive_for: int):
         if offset > 100:
