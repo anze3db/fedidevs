05ff71ddad4e526b2521c53237885002ce93dfa2
Anže Pečar <anze@pecar.me>
2025-03-09 16:19:17 +0100

    Fix csp middleware issue

--- fedidevs/settings.py
+++ fedidevs/settings.py
@@ -99,6 +99,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "csp.middleware.CSPMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
@@ -110,7 +111,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django_browser_reload.middleware.BrowserReloadMiddleware",
-    "csp.middleware.CSPMiddleware",
 ]
 
 if DEBUG:
