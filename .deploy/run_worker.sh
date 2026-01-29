f816f4f7f7d27ff0ec01f7431fa2854353a8d68d
Anže Pečar <anze@pecar.me>
2026-01-29 11:36:56 +0000

    Cleanup

--- .deploy/run_worker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-pushd "$(dirname "$0")/.."
-export NEW_RELIC_CONFIG_FILE=/var/apps/fedidevs/newrelic.ini
-/var/apps/fedidevs/.venv/bin/newrelic-admin run-program /var/apps/fedidevs/.venv/bin/python manage.py rundramatiq --processes 1 --threads 2
-popd
